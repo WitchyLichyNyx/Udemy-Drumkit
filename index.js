2a786f48bced2cf797edfa3d9b405606eccb53a3
WitchyLichyNyx <133819070+WitchyLichyNyx@users.noreply.github.com>
2023-06-30 01:28:18 +0300

    Added Keyboard presses

--- index.js
+++ index.js
@@ -2,26 +2,60 @@
 
 for (let i=0; i<document.querySelectorAll(".drum").length ;i++){
     
-    document.querySelectorAll(".drum")[i].addEventListener("click", drums);
+    document.querySelectorAll(".drum")[i].addEventListener("click", drum);
+        function drum() {
+            let  buttonInnerHTML = this.innerHTML;
 
-    function drums(){
- 
-       if (i<4){
-            let audio = new Audio('sounds/tom-' + (i+1) + '.mp3');
-            audio.play();
-        } else if (i===4) {
-            let audio = new Audio('sounds/snare.mp3');
-            audio.play();
-        } else if (i===5){
-            let audio = new Audio('sounds/crash.mp3');
-            audio.play();
-        }else if (i===6){
-            let audio = new Audio('sounds/kick-bass.mp3');
-            audio.play();
-        }
+            keypresses(buttonInnerHTML);
         
-    }
 
+
+        }
+    }  
+    document.addEventListener("keydown", function(event){keypresses(event.key)});
+    
+    function keypresses(key){
+         switch (key) {
+            case "w":
+                let tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+            break;
+
+            case "a":
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+            break;
+
+            case "s":
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+            break;
+
+            case "d":
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+            break;
+
+            case "j":
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+            break;
+
+            case "k":
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+            break;
+
+            case "l":
+                let bass = new Audio('sounds/kick-bass.mp3');
+                bass.play();
+            break;
+        
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+    
 }
 
 
